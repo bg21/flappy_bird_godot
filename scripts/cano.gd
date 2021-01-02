c9c020025e223d3be9b14f0f7c4c404bf7ab2905
devbg21 <juhcosta23@gmail.com>
2021-01-02 01:28:55 -0300

    trabalhando nos canos

---
+++ scripts/cano.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export var velocidadeCano = -100;
+
+func _ready():
+	set_process(true);
+	
+
+func _process(delta):
+	set_pos(get_pos() + Vector2(velocidadeCano * delta, 0));
+	
+	if(get_pos().x < 100):
+		queue_free();
